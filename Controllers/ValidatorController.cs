5ecfb56392b0099fa428dd264166efaa5f2d3b20
octatech <octatech@joanna.local>
2021-10-04 20:46:16 +0200

    Initial commit

---
+++ Controllers/ValidatorController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using IBANvalidator.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IBANvalidator.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValidatorController : ControllerBase
+    {
+        private readonly ILogger<ValidatorController> _logger;
+        
+        public class Input
+        {
+            public string Iban { get; set; }
+            public string IsCheckingBic { get; set; }
+        }
+
+        public ValidatorController(ILogger<ValidatorController> logger)
+        {
+            _logger  = logger;
+        }
+        
+        [HttpGet]
+        public Dictionary<string, dynamic> Get()
+        {
+            var baseUrl = HttpContext.Request.Scheme + "://" + HttpContext.Request.Host;
+            var endpointData = new Dictionary<string, dynamic>
+            {
+                {"status", "error"},
+                {"message", HttpStatusCode.MethodNotAllowed + ". Please read: " + baseUrl + "/documentation"}
+            };
+
+            return endpointData;
+        }
+
+        [HttpPost]
+        public Dictionary<string, dynamic> Run(string iban, bool isBicValidated) //[FromBody] Input data
+        {
+            IbanValidationService service = new IbanValidationService();
+            
+            service.BankAccountNumber  = iban;
+            service.IsCheckingBankCode = isBicValidated;
+
+            IbanValidationService.Validate();
+            return IbanValidationService.GenerateResult();
+            
+            //string contentType = "application/json";
+            //var httpClient = new HttpClient();
+            //HttpContent httpContent = new StringContent("Test", Encoding.UTF32, contentType);
+            //var response = httpClient.PostAsync(ingestPath, httpContent);
+        }
+    }
+}
---
+++ Services/IbanValidationService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace IBANvalidator.Services
+{
+    public class IbanValidationService
+    {
+        private static string _bankAccountNumber { get; set; }
+        private static Dictionary<string, dynamic> _bankData { get; set; }
+        private static string _bankCode { get; set; }
+        private bool _isCheckingBankCode { get; set; }
+        private static bool _isValid { get; set; }
+
+        public string BankAccountNumber
+        {
+            get => _bankAccountNumber;
+            set => _bankAccountNumber = value;
+        }
+
+        private static Dictionary<string, dynamic> BankData
+        {
+            get => _bankData;
+            set => _bankData = value;
+        }
+        
+        public bool IsCheckingBankCode
+        {
+            get => _isCheckingBankCode;
+            set => _isCheckingBankCode = value;
+        }
+
+        public IbanValidationService()
+        {
+            _bankData           = new Dictionary<string, dynamic>();
+            _bankCode           = null;
+            _isCheckingBankCode = false;
+            _isValid            = false;
+        }
+
+        public static void Validate()
+        {
+            _isValid = false;
+            
+            //TODO: validation here
+            BankData.Add("name", "Test bank");
+        }
+
+        public static Dictionary<string, dynamic> GenerateResult()
+        {
+            //TODO: load real message after process is done
+
+            var result = new Dictionary<string, dynamic>
+            {
+                {"status", "Ok"}, 
+                {"message", "Validator in progress"}, 
+                {"iban", _bankAccountNumber}, 
+                {"valid", _isValid}, 
+                {"bic", _bankCode},
+                {"bankData", _bankData}
+            };
+
+
+            return result;
+        }
+    }
+}
